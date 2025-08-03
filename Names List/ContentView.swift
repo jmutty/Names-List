4484df888dff922ed1d140814c70320f03e47141
Jeremiah Mutty <jmutty@207photo.com>
2025-08-03 19:45:13 -0400

    Initial Commit

---
+++ Names List/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Names List
+//
+//  Created by 207 Photo on 8/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Names List/Names_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Names_ListApp.swift
+//  Names List
+//
+//  Created by 207 Photo on 8/3/25.
+//
+
+import SwiftUI
+
+@main
+struct Names_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
